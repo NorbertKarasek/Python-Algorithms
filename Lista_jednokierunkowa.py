4894e96e29c31c3a4ab5c270639cf6bed6d5388a
Norbert Karasek <139477824+NorbertKarasek@users.noreply.github.com>
2024-04-27 13:58:58 +0200

    Update Lista_jednokierunkowa.py
    Added vertex removal

--- Lista_jednokierunkowa.py
+++ Lista_jednokierunkowa.py
@@ -1,12 +1,15 @@
+# Implementacja wierzchołka
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next = None):
         self.data = data
-        self.next = None
+        self.next = next
 
+# Implementacja listy
 class LinkedList:
     def __init__(self):
         self.head = None
 
+# Metoda dodania wierzchołka
     def append(self, data):
         new_node = Node(data)
         if not self.head:
@@ -17,6 +20,7 @@ class LinkedList:
             last_node = last_node.next
         last_node.next = new_node
 
+# Metoda wyświetlania listy
     def display(self):
         current_node = self.head
         while current_node:
@@ -24,13 +28,38 @@ class LinkedList:
             current_node = current_node.next
         print()
 
+# Metoda usuwania wierzchołka z warunkami
+    def remove(self, data):
+        current_node = self.head
+        # Jeśli usuwamy pierwszy element listy
+        if current_node and current_node.data == data:
+            self.head = current_node.next
+            current_node = None
+            return
+
+        prev_node = None
+        while current_node and current_node.data != data:
+            prev_node = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            return
+
+        prev_node.next = current_node.next
+        current_node = None
+
+
 # Przykład użycia:
 llist = LinkedList()
+# Dodajemy elementy
 llist.append(1)
 llist.append(2)
 llist.append(3)
 llist.append(4)
 llist.append(5)
+# Usuwamy element
+llist.remove(3)
 
+# Wyświetlamy listę
 print("Elementy listy jednokierunkowej:")
 llist.display()
